1dfda812b9259fbf6c6eba97aa9f7225eb505634
elijahmm17 <elijahmm17@gmail.com>
2019-10-02 20:57:45 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
   let task = document.querySelector('#new-task-description').value;
   let s = document.querySelectorAll('input')[1];
+  
+  s.addEventListener('click', function(e) {
+    e.preventDefault();
+  })
 });
