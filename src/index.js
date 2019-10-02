6a899c9b2220349526270b2176bc748f1bc6bdc4
elijahmm17 <elijahmm17@gmail.com>
2019-10-02 20:58:53 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  let task = document.querySelector('#new-task-description').value;
   let s = document.querySelectorAll('input')[1];
   
   s.addEventListener('click', function(e) {
